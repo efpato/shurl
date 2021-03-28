1974c16dc0bd7c4bc7eb8376649324a483b333ee
Sergey Demenok <sergey.demenok@gmail.com>
2021-03-28 22:54:04 +0300

    add auth

--- src/app/config.rs
+++ src/app/config.rs
@@ -1,11 +1,36 @@
 use super::views;
-use actix_web::web;
+use actix_web::{dev::ServiceRequest, web, Error};
+use actix_web_httpauth::extractors::{
+    bearer::{BearerAuth, Config},
+    AuthenticationError,
+};
+use actix_web_httpauth::middleware::HttpAuthentication;
+
+async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
+    let username = std::env::var("APP_USER").expect("APP_USER not found");
+    let password = std::env::var("APP_PASS").expect("APP_PASS not found");
+    let token = base64::encode(format!("{}:{}", username, password));
+
+    if credentials.token() == token.as_str() {
+        Ok(req)
+    } else {
+        let config = req
+            .app_data::<Config>()
+            .map(|data| data.clone())
+            .unwrap_or_else(Default::default);
+
+        Err(AuthenticationError::from(config).into())
+    }
+}
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/")
-            .route(web::get().to(views::index))
-            .route(web::post().to(views::create_link)),
-    )
-    .service(web::resource("/{hash}").route(web::get().to(views::follow_link)));
+    let auth = HttpAuthentication::bearer(validator);
+
+    cfg.service(web::resource("/").route(web::get().to(views::index)))
+        .service(web::resource("/{hash}").route(web::get().to(views::follow_link)))
+        .service(
+            web::resource("/api/links")
+                .wrap(auth)
+                .route(web::post().to(views::create_link)),
+        );
 }
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,22 @@
 extern crate diesel;
 
 use actix_web::{middleware, App, HttpServer};
-use dotenv::dotenv;
 use pretty_env_logger::env_logger;
-use std::env;
 
 mod app;
 mod schema;
 
 #[actix_web::main()]
 async fn main() -> std::io::Result<()> {
-    dotenv().expect("Failed to read .env file");
+    dotenv::dotenv().expect("Failed to read .env file");
 
-    env::set_var("RUST_LOG", "info");
     env_logger::builder()
         .format_timestamp(Some(env_logger::TimestampPrecision::Millis))
         .init();
 
-    let app_host = env::var("APP_HOST").expect("APP_HOST not found.");
-    let app_port = env::var("APP_PORT").expect("APP_PORT not found.");
-    let app_addr = format!("{}:{}", &app_host, &app_port);
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not found.");
+    let app_host = std::env::var("APP_HOST").expect("APP_HOST not found");
+    let app_port = std::env::var("APP_PORT").expect("APP_PORT not found");
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not found");
 
     let pool = app::db::create_db_pool(&db_url);
     let shortener = app::shortener::Shortener::new();
@@ -33,7 +29,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .configure(app::config::config)
     })
-    .bind(app_addr)?
+    .bind(format!("{}:{}", app_host, app_port))?
     .run()
     .await
 }
